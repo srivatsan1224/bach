ef06b306f2c2b78e5f2443fc188824abc3af7f25
Sudheep24 <sudheeps2004@gmail.com>
2025-02-13 21:10:15 +0530

    changes

--- client/src/services/apiService.ts
+++ client/src/services/apiService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiService = axios.create({
-  baseURL: "https://bachelors-web.onrender.com/api", // Replace with your backend URL
+  baseURL: "https://bachelors-web.onrender.com/api", // Replace with your backend UR
 });
 
 export default apiService;
